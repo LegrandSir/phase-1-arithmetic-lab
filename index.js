c7f9baf445393bf0e3492440db50b41a78fa55bc
Paul Wafula <paulwafula391@gmail.com>
2025-03-21 13:37:56 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
+let num1 = 62;
+let num2 =1;
+const multiply =num1*num2;
+let random = Math.floor(Math.random()*100) + 1;
+let num3 = 28;
+let num4 = 7;
+const mod = num3/num4;
+let max = Math.max(2,4,6,8,10,12,20)
 // Write your code here
+console.log(mod);
